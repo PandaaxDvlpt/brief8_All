d22f31dfad04caf01e4f7a004883695e32a55aa0
PandaaxY <pandaaxytb@gmail.com>
2025-05-20 11:42:18 +0200

    Images Chambre, JSON, JS, Footer

--- pages/Chambres/celebrities.api.js
+++
@@ -1,8 +0,0 @@
-[
-  {"id":1,"name":"Emma Watson","profession":"Actrice","nationality":"Britannique","knownFor":["Harry Potter","La Belle et la Bête"]},
-  {"id":2,"name":"Dwayne Johnson","profession":"Acteur / Catcheur","nationality":"Américaine","knownFor":["Fast & Furious","Jumanji"]},
-  {"id":3,"name":"Beyoncé","profession":"Chanteuse","nationality":"Américaine","knownFor":["Single Ladies","Crazy in Love"]},
-  {"id":4,"name":"Lionel Messi","profession":"Footballeur","nationality":"Argentin","knownFor":["FC Barcelone","PSG"]},
-  {"id":5,"name":"Zendaya","profession":"Actrice / Chanteuse","nationality":"Américaine","knownFor":["Euphoria","Spider-Man"]}
-  // … tu peux rajouter jusqu'à 50 si tu veux
-]
